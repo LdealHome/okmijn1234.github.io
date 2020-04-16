8c3561e3e7a296caedebbf5c1e124855f4203c68
wangsiyi <1209833057@qq.com>
2020-04-16 16:51:50 +0800

    refactor: 邀请海报全部修改成后端返回数据展示

--- src/components/community/PostersSharePopup.vue
+++ src/components/community/PostersSharePopup.vue
@@ -32,40 +32,36 @@
       nicknameText: '', // 昵称
       codeSrc: '', // 二维码
       templateIndex: -1, // 第几套海报
-      backSrc: '', // 背景图
       nicknameTextWidth: 0,
-      portraitData: {
-        portraitX: 440,
-        portraitY: 24,
-        portraitSize: 56,
-        portraitBorderColor: '#fff'
-      },
-      nicknameData: {
-        nicknameY: 40,
-        nicknameFont: '100 26px sans-serif',
-        nicknameWidth: 200
-      },
-      codeData: {
-        codeX: 520,
-        codeY: 1208,
-        codeSize: 176
-      },
+      nicknameWidth: 200, // 昵称最大宽度
+      posterArray: [], // 海报列表
       timeout: 120000,
       timer: null
     },
     /**
      * @param config {Object} 配置信息
-     * @param config.templateIndex {Number|require} 第几套海报
-     * @param config.backSrc {String|require} 背景图
      * @param config.portraitSrc {String|require} 头像
      * @param config.nicknameText {String|require} 昵称
      * @param config.codeSrc {String|require} 二维码
+     * @param config.templateIndex {Number} 第几套海报
+     * @param config.posterArray {String|require}
+     * @param config.posterArray.backSrc {String|require}  背景图
+     * @param config.posterArray.codeX {Number}  二维码x
+     * @param config.posterArray.codeY {Number}  二维码y
+     * @param config.posterArray.codeSize {Number} 二维码大小
+     * @param config.posterArray.portraitX {Number} 头像x
+     * @param config.posterArray.portraitY {Number} 头像y
+     * @param config.posterArray.portraitSize {Number} 头像大小
+     * @param config.posterArray.portraitBorderColor {String|require} 边框颜色
+     * @param config.posterArray.nicknameY {String|require} 昵称y
+     * @param config.posterArray.nicknameFont {String|require} 昵称大小
+     * @param config.posterArray.nicknameColor {String|require} // 昵称颜色
      * @param config.timeout {Number} 加载图片超时 默认10000(ms)
      * @param callback {Function} 画图成功之后执行的回调
      */
     init (config, callback) {
-      let { backSrc, portraitSrc, nicknameText, codeSrc } = config
-      if (backSrc && portraitSrc && nicknameText && codeSrc) {
+      let { portraitSrc, nicknameText, codeSrc, posterArray } = config
+      if (posterArray && portraitSrc && nicknameText && codeSrc) {
         this.data = { ...this.data, ...config }
         canvasWrapText()
         this.callback = callback
@@ -73,6 +69,7 @@
           text: '正在加载...',
           spinnerType: 'fading-circle'
         })
+        drawInvitePoster.data.posterArray = posterArray
         let image = new Image()
         image.setAttribute('crossOrigin', 'anonymous')
         image.src = drawInvitePoster.data.codeSrc
@@ -96,7 +93,7 @@
           nicknameText,
           codeSrc,
           templateIndex,
-          backSrc
+          nicknameWidth
         } = drawInvitePoster.data
         canvas.width = canvasWidth
         canvas.height = canvasHeight
@@ -114,7 +111,7 @@
         function drawBack () {
           let backImg = new Image()
           backImg.setAttribute('crossOrigin', 'anonymous')
-          backImg.src = backSrc
+          backImg.src = drawInvitePoster.data.posterArray[templateIndex].backSrc
           backImg.onerror = function () {
             $_.Indicator.close()
             clearTimeout(drawInvitePoster.data.timer)
@@ -133,15 +130,11 @@
           let {
             nicknameY,
             nicknameFont,
-            nicknameWidth
-          } = drawInvitePoster.data.nicknameData
+            nicknameColor
+          } = drawInvitePoster.data.posterArray[templateIndex]
           ctx.save()
           ctx.font = nicknameFont
-          if (templateIndex === 0) {
-            ctx.fillStyle = '#fff'
-          } else {
-            ctx.fillStyle = '#181717'
-          }
+          ctx.fillStyle = nicknameColor
           drawInvitePoster.data.nicknameTextWidth = ctx.measureText(nicknameText).width > nicknameWidth ? nicknameWidth : ctx.measureText(nicknameText).width
           ctx.textBaseline = 'top'
           ctx.wrapText(nicknameText, canvasWidth - 36 - drawInvitePoster.data.nicknameTextWidth, nicknameY, nicknameWidth, 30, 1)
@@ -156,7 +149,7 @@
             portraitY,
             portraitSize,
             portraitBorderColor
-          } = drawInvitePoster.data.portraitData
+          } = drawInvitePoster.data.posterArray[templateIndex]
           let portrait = new Image()
           portrait.setAttribute('crossOrigin', 'anonymous')
           portrait.src = portraitSrc
@@ -185,7 +178,7 @@
             codeX,
             codeY,
             codeSize
-          } = drawInvitePoster.data.codeData
+          } = drawInvitePoster.data.posterArray[templateIndex]
           ctx.save()
           ctx.fillStyle = '#c6eafd'
           ctx.fillRect(codeX - 2, codeY - 2, codeSize + 4, codeSize + 4)
@@ -218,19 +211,13 @@
     },
     data () {
       return {
-        posterInfo: {
-          portraitSrc: '', // 头像
-          nicknameText: '', // 昵称
-          codeSrc: '', // 二维码
-          templateIndex: '', // 第几套海报
-          backSrc: '' // 背景图
-        },
-        generateList: [],
+        portraitSrc: 'http://hskimgtest.smsqmx.com/upload/head_img/2020_03_07/34173cb38f07f89ddbebc2ac9128303f.png', // 头像
+        nicknameText: '你好世界', // 昵称
+        codeSrc: 'http://community-web.xy22.cn/particulars/from/30', // 二维码
+        generateList: [], // 监听生成过的图片不在重新生成
+        imgArr: [], // 生成海报的图片
         currentIndex: 0, // 海报选择的哪一个
-        backgroundList: [
-          require('../../assets/images/community/modify-img.png'),
-          require('../../assets/images/community/modify-img-back.png')
-        ], // 海报背景
+        posterArray: [], // 海报数组
         swiperOption: {
           speed: 500, // 切换速度
           watchOverflow: true, // 当没有足够的slide切换时，例如只有1个slide（非loop），swiper会失效且隐藏导航等。默认不开启这个功能。
@@ -256,7 +243,6 @@
     },
     created () {
       vm = this
-      vm.generateList = vm.backgroundList
       vm.main()
     },
     methods: {
@@ -264,15 +250,20 @@
         getSharePoster({ uid: vm.fromUid }).then(res => {
           if (res.data.code === 1) {
             let data = res.data.data
-            vm.posterInfo = {
-              portraitSrc: data.head_img,
-              nicknameText: data.nick_name,
-              codeSrc: data.qr_code
-            }
+            let memberInfo = data.member_info
+            vm.portraitSrc = memberInfo.head_img
+            vm.nicknameText = memberInfo.nick_name
 
-            vm.generateQR(this.posterInfo.codeSrc).then(res => {
-              vm.posterInfo.codeSrc = res
-              vm.posterInfo.templateIndex = vm.currentIndex
+            if (vm.posterArray.length === 0) {
+              vm.posterArray = this.transformPosterList(data.list)
+            }
+            vm.generateQR(memberInfo.qr_code).then(res => {
+              vm.codeSrc = res
+              vm.posterArray.forEach(item => {
+                vm.imgArr.push(item.backSrc)
+              })
+              // 生成第一张海报
+              vm.generateList = vm.imgArr
               vm.generatePosterArray(vm.currentIndex)
             })
           }
@@ -285,11 +276,39 @@
         if (/^data:/.test(vm.generateList[index])) { // 已经生成海报
           return
         }
-        vm.posterInfo.backSrc = vm.backgroundList[index]
-        vm.posterInfo.templateIndex = index
-        drawInvitePoster.init(this.posterInfo, function (res) {
+        vm.currentIndex = index
+        drawInvitePoster.init({
+          portraitSrc: vm.portraitSrc, // 头像
+          nicknameText: vm.nicknameText, // 昵称
+          codeSrc: vm.codeSrc, // 二维码
+          templateIndex: vm.currentIndex,
+          posterArray: vm.posterArray
+        }, function (res) {
           vm.generateList.splice(index, 1, res)
         })
+      },
+      /**
+       * 转换海报格式
+       */
+      transformPosterList (source) {
+        let list = []
+        source.forEach(item => {
+          list.push({
+            backSrc: item.img, // 背景图
+            codeX: item.qr_code_x, // 二维码x轴
+            codeY: item.qr_code_y, // 二维码y轴
+            codeSize: item.qr_code_w, // 二维码大小
+            portraitX: item.head_img_x, // 头像x轴
+            portraitY: item.head_img_y, // 头像y轴
+            portraitSize: item.head_img_w, // 头像大小
+            portraitBorderColor: item.img_border_color, // 头像边框颜色
+            nicknameY: item.nick_name_y, // 昵称y轴
+            nicknameWidth: 200, // 昵称的最大宽
+            nicknameFont: item.nick_name_size, // 昵称大小
+            nicknameColor: item.nick_name_color // 昵称颜色
+          })
+        })
+        return list
       }
     }
   }
