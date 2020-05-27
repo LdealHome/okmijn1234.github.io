ac6577d322804239fdf123c70ad2b514c9c53536
tanmingyang <1336542940@qq.com>
2020-05-27 17:33:28 +0800

    refactor: 修改资料区不展示分享记录

--- src/views/course/Course.vue
+++ src/views/course/Course.vue
@@ -690,9 +690,9 @@
             })
           }
         }
-        // 超级管理员发送的内容、分享记录、打赏记录添加到学习资料区
+        // 超级管理员发送的内容、打赏记录添加到学习资料区
         if (
-          (info.isSuperAdmin || info.type === 5 || info.type === 8) &&
+          (info.isSuperAdmin || info.type === 5) &&
           (this.mBean.state === 1 || this.mBean.studyListInfo.isLastPage)
         ) {
           this.mBean.studyListInfo.list.push(info)
